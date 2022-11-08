e65c80ad69414aaa9320e2c08e1a7ade9c84cd6c
Maurice <maurice.nzai@gmail.com>
2022-11-08 10:08:33 +0000

    update task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, swapped;
+	unsigned int i, swapped, temp;
 
 	do {
 		swapped	= 0;
@@ -20,30 +20,13 @@ void bubble_sort(int *array, size_t size)
 			/* if first element is greater than its next, swap */
 			if (array[i] > array[i + 1])
 			{
-				swap(&array[i], &array[i + 1]);
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 
 				swapped = 1;
 				print_array(array, size);
 			}
 		}
 	} while (swapped);
-}
-
-
-/**
- * swap - swaps elements of an array
- *
- * @small: small element
- * @large: large element
- *
- * Return: Nothing
- */
-
-void swap(int *small, int *large)
-{
-	int temp;
-
-	temp = *small;
-	*small = *large;
-	*large = temp;
 }
