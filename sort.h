685940ded2cebd9fd2022bbd894b406ca3c94856
Maurice <maurice.nzai@gmail.com>
2022-11-11 11:54:28 +0000

    Update Header File

--- sort.h
+++ sort.h
@@ -28,7 +28,11 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *array, int x, int y);
+void swap(int *small, int *large);
+void merge_sort(int *array, size_t size);
+void top_down_split_merge(int *array_copy, size_t start, size_t end, int *array);
+void top_down_merge(int *array, size_t start, size_t mid, size_t end, int *array_copy);
+void copy_array(int *array, size_t start, size_t end, int *array_copy);
 
 
 
